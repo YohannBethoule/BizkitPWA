b2e8b271cee85fef25a9018b2d1e3d68cc239fbe
Yohann Bethoule <yohann.bethoule@gmail.com>
2024-01-14 19:00:31 +0100

    Simple homepage

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
 </script>
 
 <template>
-    <router-view></router-view>
+    <main>
+        <router-view></router-view>
+    </main>
 </template>
 
 <style scoped>
+main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,12 +4,44 @@
 
 <template>
     <h1>Bizkit Dungeon</h1>
-    <nav class="nav">
-        <router-link to="/rules" class="nav-link">Rules</router-link>
-        <router-link to="/game" class="nav-link">Game</router-link>
-    </nav>
+    <h2>Affrontez les Seigneurs Ethyliques du Bizkit !</h2>
+    <div class="links">
+        <router-link to="/game" class="play-btn">Jouer</router-link>
+        <router-link to="/rules" class="rules-link">Voir les règles</router-link>
+    </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+h1 {
+    font-size: 4rem;
+    font-weight: 700;
+    margin-bottom: 2rem;
+}
+h2 {
+    font-size: 2rem;
+    font-weight: 400;
+    margin-bottom: 2rem;
+}
+.links {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    margin-top: 1rem;
+}
+.play-btn {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
 
+    &:hover {
+        opacity: 0.8;
+    }
+}
 </style>
