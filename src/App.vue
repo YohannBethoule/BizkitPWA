8b893636c07034978520e3a19182783865a530eb
Yohann Bethoule <yoyoteva@gmail.com>
2024-01-25 20:29:19 +0100

    Add glimpse-guru tracker (#12)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <script setup lang="ts">
+import {onMounted} from "vue";
+
+onMounted(() => {
+    function configureScript() {
+        if (typeof window.configureTracker === 'function') {
+            window.configureTracker(import.meta.env.VITE_TRACKER_API_KEY, import.meta.env.VITE_TRACKER_WEBSITE_ID, import.meta.env.VITE_TRACKER_URL)
+        }
+    }
+
+    const trackerScript = document.createElement('script');
+    trackerScript.src = "https://cdn.jsdelivr.net/gh/YohannBethoule/glimpseguru-script@master/glimpseguru.min.js";
+    trackerScript.defer = true;
+    trackerScript.onload = configureScript;
+    document.head.appendChild(trackerScript);
+})
 </script>
 
 <template>
